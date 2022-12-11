d20d8703c3d9be8a21a8c67ed85cf915eae4cbac
joy2362 <abdullahzahidjoy@gmail.com>
2022-12-11 13:45:35 +0600

    .

--- resources/views/admin/layout/partials/script.blade.php
+++ resources/views/admin/layout/partials/script.blade.php
@@ -6,6 +6,7 @@
     <script src="{{asset('asset/js/toastr.min.js')}}"></script>
     <script src="{{asset('asset/js/jquery.validate.min.js')}}"></script>
     <script src="{{asset('asset/js/additional-methods.min.js')}}"></script>
+    <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
     <script src="//cdn.ckeditor.com/4.5.9/standard/ckeditor.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.5.9/adapters/jquery.js"></script>
     <script>
@@ -33,5 +34,19 @@
         @endphp
         @endif
 
+    });
+</script>
+
+<script>
+    // Enable pusher logging - don't include this in production
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('64b5901ea9372908c0f8', {
+        cluster: 'ap1'
+    });
+
+    var channel = pusher.subscribe('notification');
+    channel.bind('activity', function(data) {
+        alert(JSON.stringify(data));
     });
 </script>
